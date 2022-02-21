776e5edbf42520dccea37638bcd25a364aea85b7
Vinicius Santos <viniciushacrinha1@gmail.com>
2022-02-21 18:10:29 -0300

    Init

---
+++ src/test/java/com/looqbok/feature/FeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looqbok.feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
